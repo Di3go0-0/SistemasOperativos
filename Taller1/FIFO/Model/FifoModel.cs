f5d43513591076ac355aae4ef2ae381b0b5b4f5c
di3go0-0 <d.rincon@utp.edu.co>
2024-08-30 21:41:05 -0500

    Modified Model to complete the algorithm

--- Taller1/FIFO/Model/FifoModel.cs
+++ Taller1/FIFO/Model/FifoModel.cs
@@ -5,6 +5,11 @@ namespace Taller1.FIFO.Model
         public string Proceso { get; set; }
         public int Rafaga { get; set; }
         public int Llegada { get; set; }
+        public int Comienzo { get; set; }
+        public int Finalizacion { get; set; }
+        public bool Ejecutado { get; set; }
+        public int TiempoEspera { get; set; }
+        public int TiempoSistema { get; set; }
 
         public FifoModel(string proceso, int rafaga, int llegada)
         {
