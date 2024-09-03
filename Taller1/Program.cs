df85c3fa56721a57cbecc4a32b54aeb2fb975d36
Di3go0-0 <d.rincon@utp.edu.co>
2024-09-02 19:26:29 -0500

    Modified Program

--- Taller1/Program.cs
+++ Taller1/Program.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿
 using Taller1.Helpers;
 using Taller1.FIFO;
+using Taller1.SJF;
 using Taller1.Model;
-using OxyPlot;
-using OxyPlot.SkiaSharp;
-
 namespace SO
 {
     public class Program
@@ -16,29 +12,14 @@ namespace SO
             ObtainData obtainData = new ObtainData(AlgorithmType.Fifo);
             List<ProcessModel> fifoData = obtainData.GetFifoData() ?? new List<ProcessModel>();
 
-            FIFO fifo = new FIFO();
+            FIFO fifo = new();
             fifo.LoadProcesos(fifoData);
+            fifo.Run();
 
-            // Mostrar por consola los datos de cada FifoModel
-            foreach (var proceso in fifo.GetProcesos())
-            {
-                Console.WriteLine($"Proceso: {proceso.Proceso}, Rafaga: {proceso.Rafaga}, Llegada: {proceso.Llegada}");
-            }
-            Console.WriteLine("Procesos ejecutados");
-            fifo.PrintModels();
-            Console.WriteLine("Tiempos");
-            fifo.PrintTiempos();
+            SFJ sjf = new();
+            sjf.LoadProcesos(fifoData);
+            sjf.Run();
 
-            // Crear y guardar la gráfica usando el nuevo método en FIFO
-            var plotModel = fifo.GeneratePlotModel();
-
-            using (var stream = File.Create("IMG/fifo_plot.png"))
-            {       
-                var pngExporter = new PngExporter { Width = 600, Height = 400 };
-                pngExporter.Export(plotModel, stream);
-            }
-
-            Console.WriteLine("Gráfica generada y guardada como fifo_plot.png");
         }
     }
 }
