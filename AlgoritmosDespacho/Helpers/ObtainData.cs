bb3bda9a1eadf2d80c81f7ebb6490459c85f91cd
Di3go0-0 <d.rincon@utp.edu.co>
2024-09-02 20:24:41 -0500

    Update Class

--- AlgoritmosDespacho/Helpers/ObtainData.cs
+++ AlgoritmosDespacho/Helpers/ObtainData.cs
@@ -6,27 +6,11 @@ using Taller.Model;
 
 namespace Taller.Helpers
 {
-    public enum AlgorithmType
-    {
-        Fifo,
-        Sjf,
-        Prioridad
-    }
-
     public class ObtainData
     {
-        public AlgorithmType SelectedAlgorithm { get; private set; }
-
-        public ObtainData(AlgorithmType algorithmType)
-        {
-            SelectedAlgorithm = algorithmType;
-        }
-
+        public ObtainData(){}
         public List<ProcessModel> GetFifoData()
         {
-            if (SelectedAlgorithm != AlgorithmType.Fifo)
-                throw new InvalidOperationException("Selected algorithm is not FIFO");
-
             string jsonData = File.ReadAllText("Data/data.json");
             return JsonConvert.DeserializeObject<List<ProcessModel>>(jsonData) ?? new List<ProcessModel>();
         }
