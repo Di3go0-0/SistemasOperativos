308d8860975b72d63e51d02c543fd6a322af0e02
Di3go0-0 <d.rincon@utp.edu.co>
2024-09-02 20:16:08 -0500

    SJF V.1

--- AlgoritmosDespacho/SJF/SFJ.cs
+++ AlgoritmosDespacho/SJF/SFJ.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using OxyPlot;
-using Taller1.Model;
+using Taller.Model;
 
-namespace Taller1.SJF
+namespace Taller.SJF
 {
     public class SFJ
     {
