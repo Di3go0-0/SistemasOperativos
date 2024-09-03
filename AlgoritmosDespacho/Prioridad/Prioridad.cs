02d237fc23426a525b5d06b67a65e00f8b7e44e7
Di3go0-0 <d.rincon@utp.edu.co>
2024-09-03 06:24:52 -0500

    Modified Prioridad

--- AlgoritmosDespacho/Prioridad/Prioridad.cs
+++ AlgoritmosDespacho/Prioridad/Prioridad.cs
@@ -70,16 +70,23 @@ namespace Taller.Prioridad
             var plotModel = this.GeneratePlotModel();
             using (var stream = System.IO.File.Create("IMG/prioridad_plot.png"))
             {
-                var pngExporter = new OxyPlot.SkiaSharp.PngExporter { Width = 600, Height = 400 };
+                var pngExporter = new OxyPlot.SkiaSharp.PngExporter { Width = 800, Height = 400 };
                 pngExporter.Export(plotModel, stream);
             }
             Console.WriteLine("Gráfica generada y guardada como prioridad_plot.png");
+        }
+        private void PrintTimes()
+        {
+            Console.WriteLine("Procesos:");
+            Console.WriteLine("Tiempo promedio de espera: " + PromedioTiempoEspera);
+            Console.WriteLine("Tiempo promedio de sistema: " + PromedioTiempoSistema);
         }
         public void Run()
         {
             this.RunProcess();
             this.CalcularTiempos();
             this.CreateIMG();
+            this.PrintTimes();
         }
 
     }
