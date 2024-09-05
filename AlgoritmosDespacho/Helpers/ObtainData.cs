8015b4a71e0064db2c74ebc794c4019efed9980f
Di3go0-0 <d.rincon@utp.edu.co>
2024-09-05 17:58:42 -0500

    Modified ObtainData.cs

--- AlgoritmosDespacho/Helpers/ObtainData.cs
+++ AlgoritmosDespacho/Helpers/ObtainData.cs
@@ -8,8 +8,9 @@ namespace Taller.Helpers
 {
     public class ObtainData
     {
-        public ObtainData(){}
-        public List<ProcessModel> GetFifoData()
+        public ObtainData() { }
+
+        public List<ProcessModel> GetData()
         {
             string jsonData = File.ReadAllText("Data/data.json");
             return JsonConvert.DeserializeObject<List<ProcessModel>>(jsonData) ?? new List<ProcessModel>();
